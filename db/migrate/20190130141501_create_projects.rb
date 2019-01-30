2b51394ba511e2024b8e1291fa25ff1978512175
github username <github email address>
2019-01-30 10:23:30 -0500

    all backend info set up

---
+++ db/migrate/20190130141501_create_projects.rb
@@ -0,0 +1,16 @@
+class CreateProjects < ActiveRecord::Migration[5.2]
+  def change
+    create_table :projects do |t|
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :thumbnail
+      t.string :demo_one
+      t.string :demo_two
+      t.string :github_one
+      t.string :gihub_two
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
