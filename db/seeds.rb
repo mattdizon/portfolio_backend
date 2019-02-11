b2ea8085b788bd1e541e2662b6bf87844c7b9eb2
github username <matthewdizon94@protonmail.com>
2019-02-11 10:47:46 -0500

    new yt link

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,7 @@ b=Framework.create({project_id:3, name:"OpenPGP.JS"})
 
 
 a = Project.create({title: "Hack The Solar System", thumbnail: "https://i.imgur.com/7TkNTwt.png",
-demo_one: "https://www.youtube.com/embed/9r7I8_tfVZg",
+demo_one: "  https://www.youtube.com/embed/oNZkU4YmHec",
 github_one: "https://github.com/HackTheSolarSystem/Hack-The-Solar-System",
 description: "During the 5th annual Hackathon in the American Museum of Natural History, the team I was working with took on the task to educate High Schoolers. Our Project involved manipulating the mass of any planet and seeing it's gravity change in its planet. A user is also able to manipulate the distance from the sun which will change the orbital speed of the planet",
 short_description: "Interactable planet where a user can change a planets mass and distance from the sun. The output will vary depending on planet and input."
